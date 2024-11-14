8498c6b938b596a5e0f715bd06466475b962137f
KuzMaxim <kuznetsov2acc@gmail.com>
2024-11-14 10:05:38 +0300

    Create rational_doctest.cpp

---
+++ prj.test/rational_doctest.cpp
@@ -0,0 +1,8 @@
+#include <rational/rational.hpp>
+#include "doctest.h"
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+TEST_CASE("Complex ariphmetic") {
+	CHECK((Real{ 1, -2 } + Real{ 1, 1 }) == Real{2, -1});
+}
